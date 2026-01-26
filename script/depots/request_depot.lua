9cd4802adbddf2f033081db306c063883de3a26e
Louliya <aqw126150@gmail.com>
2026-01-26 13:50:16 +0800

    Update request_depot.lua
    修复需求车站到管道液体消失问题

--- script/depots/request_depot.lua
+++ script/depots/request_depot.lua
@@ -454,33 +454,33 @@ function request_depot:get_current_amount()
 end
 
 function request_depot:push_fluid_hack()
-  if self.mode == request_mode.fluid then
-    local box = self:get_output_fluidbox()
-    local connected_pipelines = self.entity.fluidbox.get_connections(2)
-	for i, next_box in ipairs(connected_pipelines) do
-	  for j = 1, #next_box do
-		local next_fluid = next_box[j]
-		if (box and next_fluid and next_fluid.name == box.name) or not next_fluid then
-		  local old_amount = next_fluid and next_fluid.amount or 0
-		  local old_temperature = next_fluid and next_fluid.temperature or 0
-		  local space = next_box.get_capacity(j) - old_amount
-		  local push_amount = math.min(space, box and box.amount or 0)
-		  if push_amount > 0 then
-			next_box[j] = { name = box.name, amount = old_amount + push_amount, temperature = ((old_amount * old_temperature) + (push_amount * box.temperature)) / (old_amount + push_amount) }
-			local new_next_fluid = next_box[j]
-			local new_amount = new_next_fluid and new_next_fluid.amount or 0
-			local pushed_amount = math.max(new_amount - old_amount, 0)
-			box.amount = box.amount - pushed_amount
-			if box.amount <= 0 then box = nil end
-			self:set_output_fluidbox(box)
-			box = self:get_output_fluidbox()
-		  end
-		end
-	  end
-	end	
+  if self.mode ~= request_mode.fluid then return end
+
+  local box = self:get_output_fluidbox()
+  if not box or box.amount <= 0 then return end
+
+  local connections = self.entity.fluidbox.get_connections(2)
+  if not connections then return end
+
+  for _, pipe in pairs(connections) do
+    for i = 1, #pipe do
+      local inserted = pipe.owner.insert_fluid{
+        name = box.name,
+        amount = box.amount,
+        temperature = box.temperature
+      }
+
+      if inserted > 0 then
+        box.amount = box.amount - inserted
+        if box.amount <= 0 then
+          self:set_output_fluidbox(nil)
+          return
+        end
+        self:set_output_fluidbox(box)
+      end
+    end
   end
 end
-
 
 function request_depot:get_storage_size()
   return self:get_drone_item_count() * self:get_request_size()
@@ -663,5 +663,6 @@ function request_depot:on_config_changed()
   self:set_request_mode()
   self.fuel_on_the_way = self.fuel_on_the_way or 0
 end
+
 
 return request_depot
